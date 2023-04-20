bf66eb780791e09456dc1e9aa0984a7d9ca9c0ca
BennyXizt <64214352+BennyXizt@users.noreply.github.com>
2023-04-20 10:00:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 
 
+// 1
+
+let admin, name
+
+name = "John"
+admin = name
+alert(admin)
+
+// 2
+
+const currPlanet = "Earth"
+const currUser = "Vitalii"
+
+console.info(currPlanet, currUser)
+
+// 3 
+
+const name = prompt("Enter your name: ", null)
+const sName = prompt("Enter your Surname: ", null)
+const age = prompt("Enter your age: ", null)
+
+console.warn(name)
+console.warn(sName)
+console.warn(age)
+
+// 4
+
+const dateOfBirth = prompt("Enter your date of birthday: ", null)
+const name = prompt("Enter your name: ", null)
+
+new Date().getFullYear() - dateOfBirth > 18 ? console.log(`${name} sucessfully can walk on the street`) : console.log(`I'm sorry ${name}, but the walk for you is forbidden!`);
+
+// 5
+
+const number = prompt("Enter your number: ", null)
+number >= 10 && number <= 1100 ? console.log("Yes") : console.log("No")

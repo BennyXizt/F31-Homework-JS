8412483c701cade5f4a49ceeed829e9012773f39
BennyXizt <64214352+BennyXizt@users.noreply.github.com>
2023-05-21 21:37:19 +0200

    finished

--- index.js
+++ index.js
@@ -1,94 +1,62 @@
 
+//setTimeout
 
 //1
+setTimeout(() => {
+    console.log("Timeout after 5sec");
+}, 5000)
 
-const animal = {
-    name: "Dog",
-    type: "Chordata",
-    age: 18,
-    characteristics: {
-        color: "blue",
-        sex: "male"
-    },
-    toString() {
-        return `N: ${this.name}, A: ${this.age}, type: ${this.type} color: A${this.characteristics.color}, sex: ${this.characteristics.sex}`
-    },
-    changeAge(age) {
-        this.age = age
-        return true
-    }
+//2
+let value = 0
+setTimeout(() => {
+    value = "data changed from server"
+}, 10000)
+
+
+//setInterval
+
+//1
+setInterval(() => {
+    const time = new Date()
+    const last = 24 - time.getHours() != 0 ? `осталось ${24 - time.getHours()}h до завтра` : "уже следующий день"
+    console.log(last);
+}, 5000)
+
+//Date
+
+//1
+const currTime = () => {
+    const time = new Date()
+    console.log(`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`);
 }
 
 //2
-const car1 = {
-    brand: "BMW",
-    year: 1998,
-    characteristics: {
-        color: "blue",
-        sex: "male"
-    },
-    delete(type) {
-        delete car1[type]
-        return true
-    }
-}
-
-const car2 = {
-    brand: "BMW",
-    year: 1998,
-    characteristics: {
-        color: "blue",
-        sex: "male"
-    },
+const currTimeInYear = () => {
+    const time = new Date()
+    console.log(`${time.getDate()}.${time.getMonth()}.${time.getFullYear()}`);
 }
 
 //3
+const toNewYear = () => {
+    today = new Date()
+    
+    nextDate = new Date("January 1, 2024")
+    msPerDay = 24*60*60*1000;
+    daysLeft = Math.round((nextDate.getTime()
+     - today.getTime())/msPerDay);
+    dayname=""
+    ds=""+daysLeft
 
-const user = {
-    name: "John",
-    mail: "asfasf@yahoo.com",
-    pass: "124352",
-    checkPassword(val) {
-        return this.pass == val
-    } 
-
+    dd=parseInt(ds.substr(ds.length-1))
+    if(daysLeft>4&&daysLeft<21)dayname=" дней"
+    else
+     if(dd==1)dayname=" день"
+     else
+      if(dd==2||dd==3||dd==4)dayname=" дня"
+      else dayname=" дней"
+ 
+    console.log(`До Нового года осталось ${daysLeft+dayname}`)
 }
-
-//4
-
-const shop = {
-    name: "Party",
-    address: "Koeningsplaz. 43p",
-    item: [
-        {name: "asf", price: 22},
-        {name: "asa", price: 18},
-        {name: "asa", price: 14},
-        {name: "asa", price: 12},
-        {name: "asa", price: 199},
-        {name: "asa", price: 1},
-        {name: "asa", price: 32},
-    ],
-    sortArray() {
-        return this.item.sort((x, y) => {
-            if(x.price < y.price)
-                return 1
-            else if(x.price > y.price)
-                return -1
-            else
-                return 0
-        })
-    }
-}
-
-//5
-
-const post = {
-    address: ["Koeningsplaz. 43p", "Koeningsplaz. 42","Koeningsplaz. 41p"],
-    time: ["13:00", "15:20", "18:00"],
-    opened(time) {
-        return this.time.filter(e => e >= time)
-    }
-}
-
-
-
+currTime()
+currTimeInYear()
+toNewYear()
